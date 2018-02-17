dfdd386e42bf49f9c16c1859b38fad9c93c22ea2
saisurapaneni <silpa.surapaneni@gmail.com>
2018-02-16 19:05:34 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import  { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { AppService } from './app.service'
-
+import { AppService } from './app.service';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { ModalComponent } from './modal/modal.component';
+import { MatDialogModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent
+
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [AppService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ModalComponent]
 })
 export class AppModule { }
