21a4cffc2d55348abd6c8cec0eb768d78de1cf5c
saisurapaneni <silpa.surapaneni@gmail.com>
2018-02-16 19:04:43 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { AppService } from './app.service';
+import {MatDialog} from '@angular/material';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +14,54 @@ import { AppService } from './app.service';
 })
 
 export class AppComponent implements OnInit {
-  dealers  = [];
+  dealersList  = [];
+  dealers = [];
+  searchedDelears = [];
+  services: any;
+  selectedServices: any = [];
 
-  constructor(private appService: AppService){}
+  constructor(private appService: AppService, public dialog: MatDialog){
+    this.services = [
+      { name: 'Service Pro', selected: true },
+      { name: 'Installation Pro', selected: true },
+      { name: 'Residential Pro', selected: true },
+      { name: 'Commercial Pro', selected: false }
+    ]
+  }
+
+  openDialog(poolName) {
+    let dialogRef = this.dialog.open(ModalComponent, {
+      height: '700px',
+      width: '650px',
+      data: {name: poolName},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
     ngOnInit(){
       this.appService.getJSON()
         .subscribe(dealersResponse => this.dealers = dealersResponse,
           error => console.log(error));
+
+          //this.fitleredDealers(this.dealersList.dealers);
     }
+
+  checkIfAllSelected(item) {
+    this.selectedServices = [];
+     if(item.selected == true){
+       this.selectedServices.push(item);
+     }
+
+  }
+  fitleredDealers(dealers) {
+    this.dealers = [];
+    for(let i =0 ; i<3; i++){
+      this.dealers.push(dealers.data[i]);
+    }
+
+  }
 
 }
